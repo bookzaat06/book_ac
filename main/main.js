b5ab26cf0d33a04d77fd6c1101ba56e817d9327d
bookzaat06 <mr.bookzaat06@gmail.com>
2024-02-25 19:31:20 +0700

    ok

---
+++ main/main.js
@@ -0,0 +1,72 @@
+const { app, BrowserWindow  } = require("electron");
+const serve = require("electron-serve");
+const path = require("path");
+const { updateElectronApp } = require('update-electron-app')
+
+const appServe = app.isPackaged ? serve({
+  directory: path.join(__dirname, "../out")
+}) : null;
+
+
+updateElectronApp({
+  updateSource: {
+    type: UpdateSourceType.ElectronPublicUpdateService,
+    repo: 'github-user/repo'
+  },
+  updateInterval: '1 hour',
+  logger: require('electron-log')
+})
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js")
+    }
+  });
+
+
+
+  if (app.isPackaged) {
+    appServe(win).then(() => {
+      win.loadURL("app://-");
+    });
+  } else {
+ 
+    win.setMenuBarVisibility(false);
+    win.loadURL("http://localhost:3000");
+ 
+    win.webContents.setWindowOpenHandler(({ url }) => {
+      if (url === 'about:blank') {
+        return {
+          action: 'allow',
+          overrideBrowserWindowOptions: {
+            frame: false,
+            fullscreenable: false,
+            backgroundColor: 'black',
+            webPreferences: {
+              preload: 'my-child-window-preload-script.js'
+            }
+          }
+        }
+      }
+      return { action: 'deny' }
+    })
+
+   // win.webContents.openDevTools();
+    // win.webContents.on("did-fail-load", (e, code, desc) => {
+    //   win.webContents.reloadIgnoringCache();
+    // });
+  }
+}
+
+app.on("ready", () => {
+    createWindow();
+});
+
+app.on("window-all-closed", () => {
+    if(process.platform !== "darwin"){
+        app.quit();
+    }
+});
